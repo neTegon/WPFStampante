55259d7576afa56b27df98c6e454c51e34ec448c
Elettra Tegon <112578860+neTegon@users.noreply.github.com>
2023-12-04 17:16:01 +0100

    Add files via upload

---
+++ Pagina.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tegon.elettra._4i.stampante.models
+{
+    public class Pagina
+    {
+
+
+        //4 serbatoi di colore: CMYB 
+        //private int _c;
+        public int C;
+        public int M;
+        public int Y;
+        public int B;
+
+        //un costruttore che accetta colori specifici al massimo di valore 3
+        public Pagina(int val, string color)
+        {
+            switch (color.ToLower())
+            {
+                case "cyano":
+                    if (val > 3)
+                        C = 3;
+                    else
+                    {
+                        C = val;
+                        break;
+                    }
+                    break;
+                case "yellow":
+                    if (val > 3)
+                        Y = 3;
+                    else
+                    {
+                        Y = val;
+                        break;
+                    }
+                    break;
+                case "magenta":
+                    if (val > 3)
+                        M = 3;
+                    else
+                    {
+                        M = val;
+                        break;
+                    }
+                    break;
+                case "black":
+                    if (val > 3)
+                        B = 3;
+                    else
+                    {
+                        B = val;
+                        break;
+                    }
+                    break;
+            }
+
+        }
+
+        //un costruttore che crea una Pagina con colori random
+        public Pagina()
+        {
+            Random rnd = new Random();
+            int x = rnd.Next(100);
+            C = rnd.Next(100);
+            M = rnd.Next(100);
+            Y = rnd.Next(100);
+            B = rnd.Next(100);
+        }
+    }
+
+}
---
+++ stampante.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tegon.elettra._4i.stampante.models
+{
+    public class stampante
+    {
+        //4 serbatoi di colore: CMYB 
+        public int C {  get; set; }
+        public int M { get; set; }
+        public int Y { get; set; }
+        public int B { get; set; }
+
+
+
+        //e un cassetto di fogli(tutte property int)
+        public int Fogli { get; set; }
+        public stampante() 
+        {
+            
+            
+            C=M=B=Y=100;
+            Fogli = 200;
+        }
+
+        // un metodo bool Stampa( Pagina p ) (che torna false, se l'inchiostro non è sufficiente per stampare o non ci sono pagine)
+        public bool Stampa(Pagina p)
+        {
+            if (Fogli>0 && C>p.C && M>p.M && Y>p.Y &&  B>p.B)  
+            { 
+
+                C -= p.C;
+                M -= p.M;
+                Y -= p.Y;
+                B -= p.B;
+                Fogli--;
+
+                return true;
+            }
+            return false;
+        }
+        //un metodo int StatoInchiostro( Colore c ) che torna la quantità di inchiostro presente nei 4 serbatoi.
+        public int StatoInchiostro(string colore)
+        {
+            switch(colore.ToLower())
+            {
+                case "cyano":
+                    return C;
+                case "yellow":
+                    return Y;
+                case "magenta":
+                    return M;
+                case "black":
+                    return B;             
+            }
+            return 101;
+        }
+        // un metodo int StatoCarta() che mi ritorna la quantità di fogli nel cassetto
+        public int StatoCarta()
+        {
+            return Fogli;
+        }
+
+        //un metodo void SostituisciColore( Colore c ) che rimpiazza un serbatoio di inchiostro e lo riporta a 100
+        public void SostituisciColore(string colore)
+        {
+            switch (colore.ToLower())
+            {
+                case "cyano":
+                    C = 100;
+                    break;
+                case "yellow":
+                    Y = 100;
+                    break;
+                case "magenta":
+                    M = 100;
+                    break;
+                case "black":
+                    B = 100;
+                    break;
+            }
+        }
+        //un metodo void AggiungiCarta( int qta ) che aggiunge fogli fino ad un max di 200
+        public void AggiungiCarta(int qta)
+        {
+            Fogli = Fogli + qta;
+            if(Fogli > 200) 
+            { 
+                Fogli = 200;
+            }
+        }
+    }
+}
